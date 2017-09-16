ac818f0102e1cdc6b0b95309846fca1b8688611b
Peter Turczak <peter@turczak.de>
2017-09-16 08:58:15 +0200

    Migrated to LPC (Linear Predictive Coding) audio storage using the Talkie arduino library

--- make_sample.sh
+++ make_sample.sh
@@ -9,5 +9,6 @@
 # on other platforms. SOX and xxd from the VIM package
 say $1 -o $1.aiff
 sox $1.aiff -b 4 -r 4e3   -b 8  $1.u8 gain -n
+sox $1.aiff -r 8e3 -c 1 -b 16  $1.aif gain -n channels 1
 xxd -i $1.u8 > $1.h
 
